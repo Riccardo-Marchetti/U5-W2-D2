cf4f1aee7e0122fbdff9f2b75c88b8ae3b75a9ed
Riccardo Marchetti <marchettiriccardo04@gmail.com>
2024-04-09 16:46:25 +0200

    Blog controller

--- src/main/java/riccardo/U5W2D2/controllers/BlogPostController.java
+++ src/main/java/riccardo/U5W2D2/controllers/BlogPostController.java
@@ -1,10 +1,9 @@
 package riccardo.U5W2D2.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import riccardo.U5W2D2.entites.BlogPost;
 import riccardo.U5W2D2.services.BlogPostService;
 
@@ -20,10 +19,23 @@ public class BlogPostController {
     private List<BlogPost> getAllBlog(){
         return blogPostService.getBlogPostsList();
     }
-//    @GetMapping
-//    private BlogPost getBlogPostById(){
-//
-//    }
+    @GetMapping ("/{userId}")
+    private BlogPost getBlogPostById(@PathVariable long id){
+        return blogPostService.findBlogById(id);
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    private BlogPost postBlog(@RequestBody BlogPost body){
+        return blogPostService.saveBlogPost(body);
+    }
 
-    @
+    @PutMapping ("/blogId")
+    private BlogPost findBlogAndUpdate (@PathVariable long id, @RequestBody BlogPost body){
+        return blogPostService.findBlogByIdAndUpdate(id, body);
+    }
+
+    @DeleteMapping ("/blogId")
+    private void findBlogAndDelete (@PathVariable long id){
+        blogPostService.deleteBlogPost(id);
+    }
 }
